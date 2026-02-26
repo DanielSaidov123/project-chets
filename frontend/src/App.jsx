a22ab2869ae24e9148f5b47e1e7ff62407dff7c4
DanielSaidov123 <ddny3622@gmail.com>
2026-02-26 16:25:02 +0200

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Login } from "./pages/login/Login";
+import { SignUp } from "./pages/signup/SignUp";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Login/>
+      <div className="p-4 h-screen flax items-center justify-center">
+        <SignUp />
+      </div>
     </>
   );
 }
